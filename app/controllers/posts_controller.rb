18244fd97a7846b4cbe5eac994b6d7b2a02428f6
mostlybadfly <reachdanielnunez@gmail.com>
2014-05-28 18:11:41 -0500

    fixed model/added link to blog

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,8 +1,25 @@
 class PostsController < ApplicationController
+  def index
+    @posts = Post.all
+  end
+  
   def new
   end
   
   def create
-    render plain: params[:article].inspect
+    @post = Post.new(post_params)
+ 
+    @post.save
+    redirect_to @post
   end
+  
+  def show
+    @post = Post.find(params[:id])
+  end
+  
+private
+  def post_params
+    params.require(:post).permit(:title, :text)
+  end
+  
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20140528222018) do
+
+  create_table "articles", force: true do |t|
+    t.string   "title"
+    t.text     "text"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "posts", force: true do |t|
+    t.string   "title"
+    t.text     "text"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end
