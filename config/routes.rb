63e2b64d5582c5fe94802e5121bcf3bf403f588c
mostlybadfly <reachdanielnunez@gmail.com>
2014-05-28 16:55:00 -0500

    post submission form and post model

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 Rails.application.routes.draw do
-  resources :about
-
-  resources :welcome
+  resources :posts
+  root 'welcome#index'
+  get '/about', to: 'about#index'
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-   root 'welcome#index'
+  # root 'welcome#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
