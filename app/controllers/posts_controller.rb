9491058c418a73121b380dfbacce3a53a8bdd934
mostlybadfly <reachdanielnunez@gmail.com>
2014-06-01 17:19:23 -0500

    started with update action

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,9 +3,12 @@ class PostsController < ApplicationController
     @post = Post.new
   end
   
+  def index
+    @posts = Post.all
+  end
+  
   def create
     @post = Post.new(post_params)
- 
     if @post.save
       redirect_to @post
     else
@@ -17,12 +20,16 @@ class PostsController < ApplicationController
     @post = Post.find(params[:id])
   end
   
-  def index
-    @posts = Post.all
+  def edit
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      redirect_to @post
+    else
+      render 'edit'
+    end
   end
   
-  
-private
+  private
   def post_params
     params.require(:post).permit(:title, :text)
   end
