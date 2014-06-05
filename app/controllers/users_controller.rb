8b9e36ce7cb20796045cc579ca5cf4d9d5f71dea
mostlybadfly <reachdanielnunez@gmail.com>
2014-06-04 21:35:24 -0500

    fixed user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,20 @@
 class UsersController < ApplicationController
-  
   def index
   end
+  
+  def new
+    @user = User.new
+  end
  
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "A new user is created successfully"
+      redirect_to @user
+    else
+      render "new"
+    end
+  end
   
  
   private
