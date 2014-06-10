b72dc8c1b452eba71f3bc2e5c56ebe7f582c6b01
mostlybadfly <reachdanielnunez@gmail.com>
2014-06-09 23:02:07 -0500

    jumbotron

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,7 +6,7 @@ class PostsController < ApplicationController
   end
   
   def index
-    @posts = Post.order(created_at: :desc)
+    @posts = Post.all(:order => 'created_at DESC')
   end
   
   def create
